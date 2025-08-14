fafa765512d9e1083bf82e8fe68e78e196e16a30
Damian rogers <damianrogers51@gmail.com>
2025-08-14 09:35:43 -0700

    create seperate handle back function for navigating from notes page

--- src/app/note/[noteId]/Note.tsx
+++ src/app/note/[noteId]/Note.tsx
@@ -23,11 +23,15 @@ export default function Note({ note }: { note: Note }) {
   const [image] = useCompanion(note.display_id);
   const router = useRouter();
 
+  function handleBack() {
+    router.push('/')
+  }
+
   return (
     <div className="relative flex justify-center w-screen h-full pb-8">
       <div className="fixed top-0 left-0 right-0 bg-background sm:bg-transparent sm:px-4 sm:py-4 px-2 py-2 flex sm:border-none border-b-[.5px] border-neutral-100 z-10">
         <div className="flex items-center space-x-2 backdrop-blur-sm">
-          <button onClick={() => router.push('/')} className="text-neutral-300 hover:text-foreground transition cursor-pointer">
+          <button onClick={handleBack} className="text-neutral-300 hover:text-foreground transition cursor-pointer">
             <ChevronLeft className="w-4 h-4" />
           </button>
           {(isTitleSavePending || isEditorSavePending) && (
